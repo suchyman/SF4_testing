edd9f94c00b13858472f6fae730425b1bf6c1b89
suchyman <suchym4n@gmail.com>
2019-12-09 10:13:33 +0100

    making so much good progress

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,8 +1,9 @@
 <?php
 namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -16,9 +17,20 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show the article: "%s"',
-            $slug
-        ));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+        $users = [
+            'suchy1',
+            'suchy2',
+        ];
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+            'users' => $users,
+           
+        ]);
     }
 }
